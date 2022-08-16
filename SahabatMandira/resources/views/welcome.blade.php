6a7b2729130d29345faf0e5911574a9d7b394393
venmario <glacius2ndmioogg@gmail.com>
2022-08-16 09:27:28 +0700

    hapus komen

--- SahabatMandira/resources/views/welcome.blade.php
+++ SahabatMandira/resources/views/welcome.blade.php
@@ -1,52 +1,15 @@
-{{-- 
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.adminlte')
 
-<head>
-    <link rel="shortcut icon" href="favicon.ico">
+@section('title')
+Dashboard
+@endsection
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('page-bar')
+<div class="col-sm-6">
+    <h1 class="m-0 text-dark">Dashboard</h1>
+</div><!-- /.col -->
+@endsection
 
-    <title>Laravel</title>
+@section('contents')
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    <!-- Styles -->
-    <style>
-        .content {
-            text-align: center;
-        }
-
-        .titles {
-            font-size: 84px;
-        }
-
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-    </style>
-</head>
-
-<body> --}}
-
-    @extends('layouts.adminlte')
-
-    @section('title')
-    Dashboard
-    @endsection
-
-    @section('page-bar')
-    <div class="col-sm-6">
-        <h1 class="m-0 text-dark">Dashboard</h1>
-    </div><!-- /.col -->
-    @endsection
-
-    @section('contents')
-
-    @endsection
-    {{--</body>
-</html>--}}
+@endsection
