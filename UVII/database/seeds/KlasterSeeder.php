d3ba0f24e9b68a679afb03654ab3644ff0468826
michaelsurya <michaelsurya01@gmail.com>
2022-08-30 18:10:31 +0700

    klaster seeder

--- UVII/database/seeds/KlasterSeeder.php
+++ UVII/database/seeds/KlasterSeeder.php
@@ -12,17 +12,17 @@ class KlasterSeeder extends Seeder
     public function run()
     {
         DB::table('klaster_psikometrik')->insert([
-            [ 'nama' => 'Administrasi dan Layanan',
-                'link_kejuruan_tes_2'=>'https://www.kompas.com/skola/read/2021/08/18/154901669/administrasi-pengertian-tujuan-ciri-ciri-fungsi-dan-jenisnya',
+            ['nama' => 'Administrasi dan Layanan',
+            'link_kejuruan_tes_2'=>'http://localhost/moodle/mod/quiz/view.php?id=16',
             ],
             ['nama'=>'Agriculture',
-            'link_kejuruan_tes_2'=>'https://hot.liputan6.com/read/4693295/agrikultur-adalah-upaya-membuat-pangan-di-bidang-pertanian-ketahui-jenis-jenisnya',],
+            'link_kejuruan_tes_2'=>'http://localhost/moodle/mod/quiz/view.php?id=18',],
             ['nama'=>'Seni',
-            'link_kejuruan_tes_2'=>'',
+            'link_kejuruan_tes_2'=>'http://localhost/moodle/mod/quiz/view.php?id=19',
             ],
-            ['nama'=>'Teknik','link_kejuruan_tes_2'=>'https://www.kompas.com/edu/read/2021/11/10/131100771/17-jurusan-teknik-dengan-prospek-kerja-menjanjikan-kamu-pilih-mana?page=all',],
-            
-            
+            ['nama'=>'Teknik',
+            'link_kejuruan_tes_2'=>'http://localhost/moodle/mod/quiz/view.php?id=20',],
+                     
         ]);
     }
 }
