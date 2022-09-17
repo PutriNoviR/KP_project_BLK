7375aa54eb365b286e4d7afeb4d8fd3578db35a1
kiky <kikyfadhilah2001@gmail.com>
2022-09-17 16:02:15 +0700

    profile mentor

--- SahabatMandira/resources/views/sesipelatihan/assignTugasVerifikator.blade.php
+++ SahabatMandira/resources/views/sesipelatihan/assignTugasVerifikator.blade.php
@@ -24,7 +24,6 @@ Assign Tugas
             success: function(data) {
                 Swal.fire({
                     title: "Bukti",
-                    // imageUrl: 'https://sahabatmandira.id/storage/'+ data.data +'',
                     imageUrl: "{{ asset('storage/') }}/"+data.data,
                     imageHeight: 800,
                     imageWidth: 1300,
