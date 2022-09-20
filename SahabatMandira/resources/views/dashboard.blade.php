8e203c62523d5e8ca403014cae53e782508d8b79
Fikrivent <fikribaharuddin@gmail.com>
2022-09-20 15:41:08 +0700

    Ganti Redirect menjadi target="_blank" => IKUTI TES UVII, DETAIL PELATIHAN UGA

--- SahabatMandira/resources/views/dashboard.blade.php
+++ SahabatMandira/resources/views/dashboard.blade.php
@@ -65,7 +65,7 @@ Dashboard
 @if(count($disarankan) === 0)
 <div class="alert alert-warning" role="alert">
     <center>Anda belum mengikuti tes kejuruan, Ikuti tes untuk mengetahui pelatihan yang sesuai dengan minat kejuruan anda ! &nbsp;&nbsp;&nbsp;
-        <a href="https://ubayavii.id" class="button btn btn-primary">IKUTI TES SEKARANG !</a>
+        <a href="https://ubayavii.id" class="button btn btn-primary" target="_blank">IKUTI TES SEKARANG !</a>
     </center>
 </div>
 @else
@@ -220,7 +220,7 @@ Dashboard
                     <p>{{\Illuminate\Support\Str::limit($d->deskripsi,20,'...')}}.</p>
                 </div>
                 <div class="card-footer">
-                    <a href="{{ $d->link }}" class="button btn btn-primary">{{ __('DETAIL') }}</a>
+                    <a href="{{ $d->link }}" target="_blank" class="button btn btn-primary">{{ __('DETAIL') }}</a>
                 </div>
             </div>
         </div>
