afe1202ddb1fca9226963ec095a69d9e4437bf1f
kiky <kikyfadhilah2001@gmail.com>
2022-11-10 16:09:09 +0700

    update welcome blade

--- SahabatMandira/resources/views/pelaporan/index.blade.php
+++ SahabatMandira/resources/views/pelaporan/index.blade.php
@@ -330,7 +330,6 @@ Pelaporan
                       <label for="nama" class="col-md-12 col-form-label">{{ __('Rekomendasi Keputusan') }}</label>
                       <div class="col-md-12">
                         <select class="form-control" aria-label="Default select example" name="rekom_keputusan" value="{{$d->rekom_keputusan}}">
-                          <option value="LULUS">Diterima</option>
                           <option value="TIDAK LULUS">Tidak Diterima</option>
                           <option value="CADANGAN">Cadangan</option>
                           <option value="MENGUNDURKAN DIRI">Mengundurkan Diri</option>
@@ -547,7 +546,6 @@ Pelaporan
                       <select class="form-control" aria-label="Default select example" name="rekom_keputusan" value="{{$d->rekom_keputusan}}">
                         <option value="LULUS">Diterima</option>
                         <option value="TIDAK LULUS">Tidak Diterima</option>
-                        <option value="MENGUNDURKAN DIRI">Mengundurkan Diri</option>
                       </select>
 
                       @error('nama')
@@ -601,6 +599,7 @@ Pelaporan
           <th>Jenis Kelamin</th>
           <th>Pendidikan Terakhir</th>
           <th>INFO</th>
+          <th>UPDATE HASIL SELEKSI</th>
         </tr>
       </thead>
       <tbody id="myTable">
@@ -617,6 +616,11 @@ Pelaporan
               <i class="fas fa-info"></i>
             </a>
           </td>
+          <td>
+              <button data-toggle="modal" data-target="#modalUpdateHasilSeleksiPesertaDaftarUlang{{$d->username}}" class='btn btn-warning' onclick="modalEdit('{{$d->email_peserta}}','{{$d->sesi_pelatihans_id}}')">
+                Update Hasil Seleksi
+              </button>
+            </td>
         </tr>
         <div class="modal fade" id="modalInfoAkunPesertaDaftarUlang{{$d->username}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
 
@@ -645,6 +649,62 @@ Pelaporan
                   <label for="">Pendidikan Terakhir</label><br>
                   <p>{{$d->pendidikan_terakhir}}</p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="modal fade" id="modalUpdateHasilSeleksiPesertaDaftarUlang{{$d->username}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" id="modalContent">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalEditPelatihanPeserta">EDIT HASIL SELEKSI PESERTA</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form method="post" action="{{ route('pelatihanPesertas.update',$d->email) }}">
+                  @csrf
+                  @method('PUT')
+                  <div class="form-group">
+                    <label for="nama" class="col-md-12 col-form-label">{{ __('Email Peserta') }}</label>
+                    <div class="col-md-12">
+                      <input id="nama" type="nama" class="form-control @error('email') is-invalid @enderror" name="email" value="{{$d->email}}" readonly autocomplete="nama" autofocus>
+                      <input type="hidden" name="sesi_pelatihans_id" class="col-md-12 col-form-label" value="{{$d->sesi_pelatihans_id}}">
+                      @error('nama')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="nama" class="col-md-12 col-form-label">{{ __('Rekomendasi Keputusan') }}</label>
+                    <div class="col-md-12">
+                      <select class="form-control" aria-label="Default select example" name="rekom_keputusan" value="{{$d->rekom_keputusan}}">
+                      <option value="MENGUNDURKAN DIRI">Mengundurkan Diri</option>
+                      </select>
+
+                      @error('nama')
+                      <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                      </span>
+                      @enderror
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <input type="hidden" id="permanent" name="rekom_is_permanent" class="col-md-12 col-form-label" value="0">
+                    <div class="modal-footer">
+                      <div>
+                        <button onclick="" type="submit" id="sementara" name="action" class="btn btn-default" value="1">Simpan Sementara</button>
+                        <button onclick="myFunction(); submitFormSimpanPermanen(this);" type="submit" id="permanent" name="action" class="btn btn-primary">Simpan Permanen</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
