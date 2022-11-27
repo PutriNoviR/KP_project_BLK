46fa76ff5c1ae53e896fb9dc27cfaec5ce99b718
Fikrivent <fikribaharuddin@gmail.com>
2022-11-27 21:17:52 +0700

    Offline Script from layout.app

--- SahabatMandira/resources/views/layouts/adminlte.blade.php
+++ SahabatMandira/resources/views/layouts/adminlte.blade.php
@@ -7,7 +7,8 @@
     <title>@yield('title')</title>
     @yield('style')
 
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="{{ asset('js/jquery-3.6.1.js') }}" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
+    crossorigin="anonymous"></script>
 
     <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
