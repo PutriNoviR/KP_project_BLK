51038222bf335a6704fe6567e6d80aabc3242b84
Fikrivent <fikribaharuddin@gmail.com>
2023-01-16 15:37:21 +0700

    [Temporary] Disable bot Filter (Login Controller)

--- SahabatMandira/app/Http/Controllers/Auth/LoginController.php
+++ SahabatMandira/app/Http/Controllers/Auth/LoginController.php
@@ -97,12 +97,12 @@ class LoginController extends Controller
                     Session::flash('recaptcha', 'please refresh browser');
 
                 }
-                else{
-                    if($response->score < 0.5){
-                        $fail('Please refresh browser and try again!');
-                        Session::flash('recaptcha', 'please refresh browser');
-                    }
-                }
+                // else{
+                //     if($response->score < 0.5){
+                //         $fail('Please refresh browser and try again!');
+                //         Session::flash('recaptcha', 'please refresh browser');
+                //     }
+                // }
             }
         ]);
     }
